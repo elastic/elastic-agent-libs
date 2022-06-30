elastic/elastic-agent-libs

commit 5bdd25e7a3045cd6951236069c362a9868fe9895

mapstr: optimyze Clone (#64)
Reduce the ammount of allocations when using Clone().

name     old time/op    new time/op    delta
Clone-8    8.95µs ± 0%    5.51µs ± 0%   ~     (p=1.000 n=1+1)

name     old alloc/op   new alloc/op   delta
Clone-8    4.99kB ± 0%    2.50kB ± 0%   ~     (p=1.000 n=1+1)

name     old allocs/op  new allocs/op  delta
Clone-8      64.0 ± 0%      33.0 ± 0%   ~     (p=1.000 n=1+1)

mapstr/mapstr.go
@@ -152,13 +152,13 @@ func (m M) CopyFieldsTo(to M, key string) error {
 // Clone returns a copy of the M. It recursively makes copies of inner
 // maps.
 func (m M) Clone() M {
-	result := M{}
+	result := make(M, len(m))
 
-	for k, v := range m {
-		if innerMap, ok := tryToMapStr(v); ok {
-			v = innerMap.Clone()
+	for k := range m {
+		if innerMap, ok := (m[k]).(M); ok {
+			result[k] = innerMap.Clone()
 		}
-		result[k] = v
+		result[k] = m[k]
 	}
 
 	return result

mapstr/mapstr_test.go
@@ -356,6 +356,37 @@ func TestClone(t *testing.T) {
 	assert.Equal(M{"c31": 1, "c32": 2}, c["c3"])
 }
 
+func BenchmarkClone(b *testing.B) {
+	assert := assert.New(b)
+
+	m := M{
+		"c1": 1,
+		"c2": 2,
+		"c3": M{
+			"c31": 1,
+			"c32": 2,
+			"c33": 3,
+			"c34": 4,
+			"c35": 5,
+			"c36": 6,
+			"c37": 7,
+			"c38": 8,
+			"c39": 9,
+		},
+		"c4": 4,
+		"c5": 5,
+		"c6": 6,
+		"c7": 7,
+		"c8": 8,
+		"c9": 9,
+	}
+
+	for i := 0; i < b.N; i++ {
+		c := m.Clone()
+		assert.Equal(m, c)
+	}
+}
+
 func TestString(t *testing.T) {
 	type io struct {
 		Input  M

commit 1ee8e9c143472742ee257eacd14eb1bc75e66f4d

Fix bug introduced by mapstr optimization.
The bug was introduced by
https://github.com/elastic/elastic-agent-libs/pull/64.

Add a test to catch the bug that slipped through.

mapstr/mapstr.go
@@ -157,8 +157,9 @@ func (m M) Clone() M {
 	for k := range m {
 		if innerMap, ok := (m[k]).(M); ok {
 			result[k] = innerMap.Clone()
+		} else {
+			result[k] = m[k]
 		}
-		result[k] = m[k]
 	}
 
 	return result

mapstr/mapstr_test.go
@@ -343,7 +343,7 @@ func TestMapStrGetValue(t *testing.T) {
 func TestClone(t *testing.T) {
 	assert := assert.New(t)
 
-	m := M{
+	original := M{
 		"c1": 1,
 		"c2": 2,
 		"c3": M{
@@ -352,8 +352,36 @@ func TestClone(t *testing.T) {
 		},
 	}
 
-	c := m.Clone()
-	assert.Equal(M{"c31": 1, "c32": 2}, c["c3"])
+	// Clone the original mapstr and then increment every value in it. Ensures the test will fail if
+	// the cloned mapstr kept a reference to any part of the original.
+	cloned := original.Clone()
+	incrementMapstrValues(original)
+
+	// Ensure that the cloned copy is as expected and no longer matches the original mapstr.
+	assert.Equal(
+		M{
+			"c1": 1,
+			"c2": 2,
+			"c3": M{
+				"c31": 1,
+				"c32": 2,
+			},
+		},
+		cloned,
+	)
+	assert.NotEqual(cloned, original)
+}
+
+func incrementMapstrValues(m M) {
+	for k := range m {
+		switch v := m[k].(type) {
+		case int:
+			m[k] = v + 1
+		case M:
+			incrementMapstrValues(m[k].(M))
+		}
+
+	}
 }
 
 func BenchmarkClone(b *testing.B) {