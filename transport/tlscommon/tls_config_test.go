2edaf345d4f49de78d6699f12d664a634eea4b35
Tiago Queiroz <tiago.queiroz@elastic.co>
2023-12-19 17:55:11 +0100

    Update TLS certificates in tests
    Some TLS certificates used in tests expired, this commit fixes it by
    generating the certificates and, if needed, calculating the
    fingerprint on each test.
    
    This will prevent future CI failures and reduce the maintenance
    burden.

--- transport/tlscommon/tls_config_test.go
+++ transport/tlscommon/tls_config_test.go
@@ -18,15 +18,14 @@
 package tlscommon
 
 import (
+	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/pem"
+	"encoding/hex"
 	"errors"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,15 +33,10 @@ import (
 )
 
 func TestMakeVerifyServerConnection(t *testing.T) {
-	testCerts := openTestCerts(t)
+	testCerts := genTestCerts(t)
 
-	testCA, errs := LoadCertificateAuthorities([]string{
-		filepath.Join("testdata", "ca.crt"),
-		filepath.Join("testdata", "cacert.crt"),
-	})
-	if len(errs) > 0 {
-		t.Fatalf("failed to load test certificate authorities: %+v", errs)
-	}
+	certPool := x509.NewCertPool()
+	certPool.AddCert(testCerts["ca"])
 
 	testcases := map[string]struct {
 		verificationMode TLSVerificationMode
@@ -64,7 +58,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"default verification with certificates when required with expired cert": {
 			verificationMode: VerifyFull,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["expired"]},
 			serverName:       "",
 			expectedCallback: true,
@@ -73,7 +67,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"default verification with certificates when required with incorrect server name in cert": {
 			verificationMode: VerifyFull,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["correct"]},
 			serverName:       "bad.example.com",
 			expectedCallback: true,
@@ -82,7 +76,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"default verification with certificates when required with correct cert": {
 			verificationMode: VerifyFull,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["correct"]},
 			serverName:       "localhost",
 			expectedCallback: true,
@@ -91,7 +85,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"default verification with certificates when required with correct wildcard cert": {
 			verificationMode: VerifyFull,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["wildcard"]},
 			serverName:       "hello.example.com",
 			expectedCallback: true,
@@ -100,7 +94,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"certificate verification with certificates when required with correct cert": {
 			verificationMode: VerifyCertificate,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["correct"]},
 			serverName:       "localhost",
 			expectedCallback: true,
@@ -109,7 +103,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"certificate verification with certificates when required with expired cert": {
 			verificationMode: VerifyCertificate,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["expired"]},
 			serverName:       "localhost",
 			expectedCallback: true,
@@ -118,7 +112,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"certificate verification with certificates when required with incorrect server name in cert": {
 			verificationMode: VerifyCertificate,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["correct"]},
 			serverName:       "bad.example.com",
 			expectedCallback: true,
@@ -127,7 +121,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"strict verification with certificates when required with correct cert": {
 			verificationMode: VerifyStrict,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["correct"]},
 			serverName:       "localhost",
 			expectedCallback: false,
@@ -136,7 +130,7 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 		"default verification with certificates when required with cert signed by unknown authority": {
 			verificationMode: VerifyFull,
 			clientAuth:       tls.RequireAndVerifyClientCert,
-			certAuthorities:  testCA,
+			certAuthorities:  certPool,
 			peerCerts:        []*x509.Certificate{testCerts["unknown authority"]},
 			serverName:       "",
 			expectedCallback: true,
@@ -191,11 +185,13 @@ func TestMakeVerifyServerConnection(t *testing.T) {
 }
 
 func TestTrustRootCA(t *testing.T) {
-	certs := openTestCerts(t)
+	certs := genTestCerts(t)
 
 	nonEmptyCertPool := x509.NewCertPool()
 	nonEmptyCertPool.AddCert(certs["wildcard"])
 	nonEmptyCertPool.AddCert(certs["unknown authority"])
+
+	fingerprint := getFingerprint(certs["ca"])
 
 	testCases := []struct {
 		name                 string
@@ -207,21 +203,21 @@ func TestTrustRootCA(t *testing.T) {
 	}{
 		{
 			name:                 "RootCA cert matches the fingerprint and is added to cfg.RootCAs",
-			caTrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
-			peerCerts:            []*x509.Certificate{certs["es-leaf"], certs["es-root-ca"]},
+			caTrustedFingerprint: fingerprint,
+			peerCerts:            []*x509.Certificate{certs["correct"], certs["ca"]},
 			expectedRootCAsLen:   1,
 		},
 		{
 			name:                 "RootCA cert doesn not matche the fingerprint and is not added to cfg.RootCAs",
-			caTrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
-			peerCerts:            []*x509.Certificate{certs["es-leaf"], certs["es-root-ca"]},
+			caTrustedFingerprint: fingerprint,
+			peerCerts:            []*x509.Certificate{certs["correct"], certs["ca"]},
 			expectedRootCAsLen:   0,
 		},
 		{
 			name:                 "non empty CertPool has the RootCA added",
 			rootCAs:              nonEmptyCertPool,
-			caTrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
-			peerCerts:            []*x509.Certificate{certs["es-leaf"], certs["es-root-ca"]},
+			caTrustedFingerprint: fingerprint,
+			peerCerts:            []*x509.Certificate{certs["correct"], certs["ca"]},
 			expectedRootCAsLen:   3,
 		},
 		{
@@ -263,7 +259,8 @@ func TestTrustRootCA(t *testing.T) {
 }
 
 func TestMakeVerifyConnectionUsesCATrustedFingerprint(t *testing.T) {
-	testCerts := openTestCerts(t)
+	testCerts := genTestCerts(t)
+	fingerprint := getFingerprint(testCerts["ca"])
 
 	testcases := map[string]struct {
 		verificationMode     TLSVerificationMode
@@ -276,35 +273,35 @@ func TestMakeVerifyConnectionUsesCATrustedFingerprint(t *testing.T) {
 	}{
 		"CATrustedFingerprint and verification mode:VerifyFull": {
 			verificationMode:     VerifyFull,
-			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:            []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:           "localhost",
 			expectedCallback:     true,
-			CATrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+			CATrustedFingerprint: fingerprint,
 		},
 		"CATrustedFingerprint and verification mode:VerifyCertificate": {
 			verificationMode:     VerifyCertificate,
-			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:            []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:           "localhost",
 			expectedCallback:     true,
-			CATrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
+			CATrustedFingerprint: fingerprint,
 		},
 		"CATrustedFingerprint and verification mode:VerifyStrict": {
 			verificationMode:     VerifyStrict,
-			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:            []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:           "localhost",
 			expectedCallback:     true,
-			CATrustedFingerprint: "e83171aa133b2b507e057fe091e296a7e58e9653c2b88d203b64a47eef6ec62b",
-			CASHA256:             []string{Fingerprint(testCerts["es-leaf"])},
+			CATrustedFingerprint: fingerprint,
+			CASHA256:             []string{Fingerprint(testCerts["correct"])},
 		},
 		"CATrustedFingerprint and verification mode:VerifyNone": {
 			verificationMode: VerifyNone,
-			peerCerts:        []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:        []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:       "localhost",
 			expectedCallback: false,
 		},
 		"invalid CATrustedFingerprint and verification mode:VerifyFull returns error": {
 			verificationMode:     VerifyFull,
-			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:            []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:           "localhost",
 			expectedCallback:     true,
 			CATrustedFingerprint: "INVALID HEX ENCODING",
@@ -312,7 +309,7 @@ func TestMakeVerifyConnectionUsesCATrustedFingerprint(t *testing.T) {
 		},
 		"invalid CATrustedFingerprint and verification mode:VerifyCertificate returns error": {
 			verificationMode:     VerifyCertificate,
-			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:            []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:           "localhost",
 			expectedCallback:     true,
 			CATrustedFingerprint: "INVALID HEX ENCODING",
@@ -320,12 +317,12 @@ func TestMakeVerifyConnectionUsesCATrustedFingerprint(t *testing.T) {
 		},
 		"invalid CATrustedFingerprint and verification mode:VerifyStrict returns error": {
 			verificationMode:     VerifyStrict,
-			peerCerts:            []*x509.Certificate{testCerts["es-leaf"], testCerts["es-root-ca"]},
+			peerCerts:            []*x509.Certificate{testCerts["correct"], testCerts["ca"]},
 			serverName:           "localhost",
 			expectedCallback:     true,
 			CATrustedFingerprint: "INVALID HEX ENCODING",
 			expectingError:       true,
-			CASHA256:             []string{Fingerprint(testCerts["es-leaf"])},
+			CASHA256:             []string{Fingerprint(testCerts["correct"])},
 		},
 	}
 
@@ -410,7 +407,8 @@ func TestMakeVerifyServerConnectionForIPs(t *testing.T) {
 				false,
 				test.commonName,
 				test.dnsNames,
-				test.ips)
+				test.ips,
+				false)
 			if err != nil {
 				t.Fatalf("cannot generate peer certificate: %s", err)
 			}
@@ -585,7 +583,7 @@ func TestVerificationMode(t *testing.T) {
 
 	for name, test := range testcases {
 		t.Run(name, func(t *testing.T) {
-			certs, err := genSignedCert(caCert, x509.KeyUsageCertSign, false, test.commonName, test.dnsNames, test.ips)
+			certs, err := genSignedCert(caCert, x509.KeyUsageCertSign, false, test.commonName, test.dnsNames, test.ips, false)
 			if err != nil {
 				t.Fatalf("could not generate certificates: %s", err)
 			}
@@ -678,30 +676,111 @@ func startTestServer(t *testing.T, serverAddr string, serverCerts []tls.Certific
 	return *serverURL
 }
 
-func openTestCerts(t testing.TB) map[string]*x509.Certificate {
-	t.Helper()
-	certs := make(map[string]*x509.Certificate, 0)
+func getFingerprint(cert *x509.Certificate) string {
+	caSHA256 := sha256.Sum256(cert.Raw)
+	return hex.EncodeToString(caSHA256[:])
+}
 
-	for testcase, certname := range map[string]string{
-		"expired":           "tls.crt",
-		"unknown authority": "unsigned_tls.crt",
-		"correct":           "client1.crt",
-		"wildcard":          "server.crt",
-		"es-leaf":           "es-leaf.crt",
-		"es-root-ca":        "es-root-ca-cert.crt",
-	} {
-
-		certBytes, err := ioutil.ReadFile(filepath.Join("testdata", certname))
-		if err != nil {
-			t.Fatalf("reading file %q: %+v", certname, err)
-		}
-		block, _ := pem.Decode(certBytes)
-		testCert, err := x509.ParseCertificate(block.Bytes)
-		if err != nil {
-			t.Fatalf("parsing certificate %q: %+v", certname, err)
-		}
-		certs[testcase] = testCert
+func genTestCerts(t *testing.T) map[string]*x509.Certificate {
+	ca, err := genCA()
+	if err != nil {
+		t.Fatalf("cannot generate root CA: %s", err)
 	}
+
+	unknownCA, err := genCA()
+	if err != nil {
+		t.Fatalf("cannot generate second root CA: %s", err)
+	}
+
+	certs := map[string]*x509.Certificate{
+		"ca": ca.Leaf,
+	}
+
+	certData := map[string]struct {
+		ca       tls.Certificate
+		keyUsage x509.KeyUsage
+		isCA     bool
+		dnsNames []string
+		ips      []net.IP
+		expired  bool
+	}{
+		"wildcard": {
+			ca:       ca,
+			keyUsage: x509.KeyUsageDigitalSignature,
+			isCA:     false,
+			dnsNames: []string{"*.example.com"},
+		},
+		"correct": {
+			ca:       ca,
+			keyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+			isCA:     false,
+			dnsNames: []string{"localhost"},
+			// IPV4 and IPV6
+			ips: []net.IP{{127, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		},
+		"unknown authority": {
+			ca:       unknownCA,
+			keyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+			isCA:     false,
+			dnsNames: []string{"localhost"},
+			// IPV4 and IPV6
+			ips: []net.IP{{127, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		},
+		"expired": {
+			ca:       ca,
+			keyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+			isCA:     false,
+			dnsNames: []string{"localhost"},
+			// IPV4 and IPV6
+			ips:     []net.IP{{127, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+			expired: true,
+		},
+	}
+
+	for certName, data := range certData {
+		cert, err := genSignedCert(
+			data.ca,
+			data.keyUsage,
+			data.isCA,
+			certName,
+			data.dnsNames,
+			data.ips,
+			data.expired,
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		certs[certName] = cert.Leaf
+	}
+
+	// If for any reason there is a need to debug
+	// or inspect those certificates, just uncomment the
+	// following block. It will write all generated
+	// certificates to testdata/debug
+
+	// mapName := map[string]string{
+	// 	"ca":                "ca.crt",
+	// 	"correct":           "client1.crt",
+	// 	"expired":           "tls.crt",
+	// 	"unknown authority": "unsigned_tls.crt",
+	// 	"wildcard":          "server.crt",
+	// }
+
+	// for certName, cert := range certs {
+	// 	certPEM := new(bytes.Buffer)
+	// 	pem.Encode(certPEM, &pem.Block{
+	// 		Type:  "CERTIFICATE",
+	// 		Bytes: cert.Raw,
+	// 	})
+
+	// 	serverCertFile, err := os.Create(filepath.Join("testdata", "debug", mapName[certName]))
+	// 	if err != nil {
+	// 		t.Fatalf("creating file to write server certificate: %v", err)
+	// 	}
+	// 	if _, err := serverCertFile.Write(certPEM.Bytes()); err != nil {
+	// 		t.Fatalf("writing server certificate: %v", err)
+	// 	}
+	// }
 
 	return certs
 }
