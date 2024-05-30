565c2d62a3de8f037fac8ea566ae25c7ef5f9b98
michel-laterman <michel.laterman@elastic.co>
2024-05-30 13:15:32 -0700

    Add some error diagnostics
    Add some error parsing to HTTPSettings.DiagRequests to attempt to give a
    human readable statement about the cause of any errors when gathering
    request diagnostics.

--- transport/httpcommon/diag.go
+++ transport/httpcommon/diag.go
@@ -20,10 +20,16 @@ package httpcommon
 import (
 	"bytes"
 	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httptrace"
 	"net/textproto"
+	"net/url"
 
 	"github.com/elastic/elastic-agent-libs/transport/tlscommon"
 )
@@ -112,7 +118,10 @@ func (settings *HTTPTransportSettings) DiagRequests(reqs []*http.Request, opts .
 			logger.Printf("Request %d to %s starting", i, req.URL.String())
 			req = req.WithContext(httptrace.WithClientTrace(req.Context(), ct))
 			if resp, err := rt.RoundTrip(req); err != nil {
-				logger.Printf("request %d error: %v", i, err)
+				logger.Printf("request %d error: %s", i, diagError(err))
+			} else if isGoHTTPResp(resp) {
+				resp.Body.Close()
+				logger.Printf("request %d error: HTTP request sent to HTTPS server.", i)
 			} else {
 				resp.Body.Close()
 				logger.Printf("request %d successful. status=%d", i, resp.StatusCode)
@@ -120,4 +129,70 @@ func (settings *HTTPTransportSettings) DiagRequests(reqs []*http.Request, opts .
 		}
 		return b.Bytes()
 	}
+}
+
+// isGoHTTPResp detects if the response is one that a go http.Server sends if an HTTP request is made to an HTTPS server.
+// non Go servers may return a net.OpError instead.
+func isGoHTTPResp(r *http.Response) bool {
+	if r.StatusCode != http.StatusBadRequest {
+		return false
+	}
+	p, err := io.ReadAll(r.Body)
+	if err != nil {
+		return false
+	}
+	return string(p) == "Client sent an HTTP request to an HTTPS server.\n"
+}
+
+// diagError tries to diagnose the error and return a cause/possible cause in a human readable format.
+// If no matching errors are found err.Error is returned.
+func diagError(err error) string {
+	// client does not support server algorithm
+	if errors.Is(err, x509.ErrUnsupportedAlgorithm) {
+		return fmt.Sprintf("%v: caused by client does not support server's signature algorithm.", err)
+	}
+
+	// a *net.OpError could indicate an HTTP request made to an HTTPS server
+	var netErr *net.OpError
+	if errors.As(err, &netErr) {
+		if netErr.Err.Error() == "read: connection reset by peer" {
+			return fmt.Sprintf("%v: possible cause: HTTP schema used for HTTPS server.", netErr)
+		}
+	}
+
+	// Client does not have CA that matches server cert
+	var unknownAuthErr x509.UnknownAuthorityError
+	if errors.As(err, &unknownAuthErr) {
+		return fmt.Sprintf("%v: caused by no trusted client CA.", err)
+	}
+
+	// CA is ok but the server's cert is not.
+	var certValidErr x509.CertificateInvalidError
+	if errors.As(err, &certValidErr) {
+		return fmt.Sprintf("%v: caused by invalid server certificate.", certValidErr)
+	}
+
+	// cert validation error can indicate that a custom CA needs to be used
+	var tlsErr *tls.CertificateVerificationError
+	if errors.As(err, &tlsErr) {
+		return fmt.Sprintf("%v: possible cause: client TLS settings incorrect.", tlsErr)
+	}
+
+	// keep unwrapping to url.Error as the last error as other failures can be embedded in a url.Error
+	// Can detect if an HTTPS request is made to an HTTP server
+	var uErr *url.Error
+	if errors.As(err, &uErr) {
+		switch uErr.Err.Error() {
+		case "http: server gave HTTP response to HTTPS client":
+			return fmt.Sprintf("%v: caused by using HTTPS schema on HTTP server.", uErr)
+		case "remote error: tls: certificate required":
+			return fmt.Sprintf("%v: caused by missing mTLS client cert.", uErr)
+		case "remote error: tls: expired certificate":
+			return fmt.Sprintf("%v: caused by expired mTLS client cert.", uErr)
+		case "remote error: tls: bad certificate":
+			return fmt.Sprintf("%v: caused by invalid mTLS client cert, does the server trust the CA used for the client cert?.", uErr)
+		}
+	}
+
+	return err.Error()
 }
