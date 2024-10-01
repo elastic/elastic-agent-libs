11ab9fd1d25a90af685c4f7ca99c2fd24ab698f0
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2024-10-01 14:08:42 -0300

    remove os.CreateTemp

--- file/fileinfo_test.go
+++ file/fileinfo_test.go
@@ -34,13 +34,14 @@ import (
 )
 
 func TestStat(t *testing.T) {
-	f, err := os.CreateTemp("", "teststat")
+	tmpDir := t.TempDir()
+	f, err := os.Create(filepath.Join(tmpDir, "teststat"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove(f.Name())
+	defer f.Close()
 
-	link := filepath.Join(os.TempDir(), "teststat-link")
+	link := filepath.Join(tmpDir, "teststat-link")
 	if err := os.Symlink(f.Name(), link); err != nil {
 		t.Fatal(err)
 	}
