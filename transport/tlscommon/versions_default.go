88828efc72cb24123c717405b9a44442e592bdeb
kruskall <99559985+kruskall@users.noreply.github.com>
2024-12-16 18:37:35 +0100

    Update versions_default.go

--- transport/tlscommon/versions_default.go
+++ transport/tlscommon/versions_default.go
@@ -31,7 +31,7 @@ const (
 
 var (
 	// TLSVersionMin is the min TLS version supported.
-	TLSVersionMin = TLSVersion12
+	TLSVersionMin = TLSVersion11
 
 	// TLSVersionMax is the max TLS version supported.
 	TLSVersionMax = TLSVersion13
