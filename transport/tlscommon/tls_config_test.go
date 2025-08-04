d7bd62f9492d3847ea9e5b075735dc9a9a2e0268
Khushi Jain <khushi.jain@elastic.co>
2025-08-04 14:51:26 +0530

    fix test conditions

--- transport/tlscommon/tls_config_test.go
+++ transport/tlscommon/tls_config_test.go
@@ -674,6 +674,8 @@ func TestVerificationMode(t *testing.T) {
 				if err != nil {
 					// We got the expected error, no need to check the status code
 					return
+				} else {
+					t.Fatalf("expected error, got: %v", err)
 				}
 			}
 
