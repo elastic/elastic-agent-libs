782242e0725471021853a810107b137af232ee90
Anderson Queiroz <anderson.queiroz@elastic.co>
2025-08-12 15:23:33 +0200

    make safe for nil reg

--- monitoring/metrics.go
+++ monitoring/metrics.go
@@ -43,8 +43,12 @@ type Int struct{ i atomic.Int64 }
 // variable will be available via expvars package as well, but can not be removed
 // anymore.
 func NewInt(r *Registry, name string, opts ...Option) *Int {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
@@ -80,8 +84,12 @@ type Uint struct{ u atomic.Uint64 }
 // variable will be available via expvars package as well, but can not be removed
 // anymore.
 func NewUint(r *Registry, name string, opts ...Option) *Uint {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
@@ -120,8 +128,12 @@ type Float struct{ f atomic.Uint64 }
 // variable will be available via expvars package as well, but can not be removed
 // anymore.
 func NewFloat(r *Registry, name string, opts ...Option) *Float {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
@@ -164,8 +176,12 @@ type Bool struct{ f atomic.Bool }
 // variable will be available via expvars package as well, but can not be removed
 // anymore.
 func NewBool(r *Registry, name string, opts ...Option) *Bool {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
@@ -200,8 +216,12 @@ type String struct {
 // variable will be available via expvars package as well, but can not be removed
 // anymore.
 func NewString(r *Registry, name string, opts ...Option) *String {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
@@ -254,8 +274,12 @@ type Func struct {
 }
 
 func NewFunc(r *Registry, name string, f func(Mode, Visitor), opts ...Option) *Func {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
@@ -300,8 +324,12 @@ type Timestamp struct {
 
 // NewTimestamp creates and registers a new timestamp variable.
 func NewTimestamp(r *Registry, name string, opts ...Option) *Timestamp {
-	r.txMu.Lock()
-	defer r.txMu.Unlock()
+	rr := r
+	if rr == nil {
+		rr = Default
+	}
+	rr.txMu.Lock()
+	defer rr.txMu.Unlock()
 
 	existingVar, r := setupMetric(r, name, opts)
 	if existingVar != nil {
