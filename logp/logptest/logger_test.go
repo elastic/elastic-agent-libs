a865591db7596ce3a2bf26dd5fd394f8b9e55bc2
Tiago Queiroz <tiago.queiroz@elastic.co>
2025-10-27 15:45:11 -0400

    Add more tests

--- logp/logptest/logger_test.go
+++ logp/logptest/logger_test.go
@@ -2,9 +2,14 @@ package logptest
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 )
@@ -57,22 +62,6 @@ func TestNewFileLogger(t *testing.T) {
 	})
 }
 
-func TestLoggerFileIsRemoved(t *testing.T) {
-	toBeDeletedLogFile := ""
-	// Use a subtest so it can finish and succeed before
-	// we check if the log file has been removed
-	t.Run("log file is removed", func(t *testing.T) {
-		logger := NewFileLogger(t, "")
-		logger.Info("foo")
-		toBeDeletedLogFile = logger.logFile.Name()
-	})
-
-	_, err := os.Stat(toBeDeletedLogFile)
-	if !errors.Is(err, os.ErrNotExist) {
-		t.Error("the log file must be removed after a test passes")
-	}
-}
-
 func assertLogFormat(t *testing.T, path string) {
 	t.Helper()
 
@@ -106,5 +95,112 @@ func assertLogFormat(t *testing.T, path string) {
 		if entry.Message == "" {
 			t.Error("message cannot be empty")
 		}
+	}
+}
+
+func TestLoggerCustomDir(t *testing.T) {
+	dir := t.TempDir()
+	logger := NewFileLogger(t, dir)
+	logger.Info("foo")
+
+	files, err := filepath.Glob(filepath.Join(dir, "testing-logger-*.log"))
+	if err != nil {
+		t.Fatalf("cannot resolve glob: %s", err)
+	}
+
+	if len(files) != 1 {
+		t.Fatalf("expecting a single log file, got: %d", len(files))
+	}
+}
+
+func TestLoggerFileIsRemoved(t *testing.T) {
+	toBeDeletedLogFile := ""
+	// Use a subtest so it can finish and succeed before
+	// we check if the log file has been removed
+	t.Run("log file is removed", func(t *testing.T) {
+		logger := NewFileLogger(t, "")
+		logger.Info("foo")
+		toBeDeletedLogFile = logger.logFile.Name()
+	})
+
+	_, err := os.Stat(toBeDeletedLogFile)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Error("the log file must be removed after a test passes")
+	}
+}
+
+func TestLoggerFileIsKeptOnTestFailure(t *testing.T) {
+	if os.Getenv("INNER_TEST") == "1" {
+		// We're inside the subprocess, use Logger and make it fail the test,
+		// so the actual test can ensure it is behaving correctly
+		logger := NewFileLogger(t, "")
+		logger.Info("foo")
+		logger.Warn("foo")
+		logger.Debug("foo")
+		logger.LogContains(t, "bar")
+
+		return
+	}
+
+	//nolint:gosec // This is intentionally running a subprocess
+	cmd := exec.CommandContext(
+		t.Context(),
+		os.Args[0],
+		fmt.Sprintf("-test.run=^%s$",
+			t.Name()),
+		"-test.v")
+	cmd.Env = append(cmd.Env, "INNER_TEST=1")
+
+	d, cmdErr := cmd.CombinedOutput()
+	if cmdErr != nil {
+		// The test ran by cmd will fail and retrun 1 as the exit code. So we only
+		// print the error if the main test fails.
+		defer func() {
+			if t.Failed() {
+				t.Errorf("the test process returned an error (this is expected in on a normal test execution): %s", cmdErr)
+			}
+		}()
+	}
+
+	var path string
+	sc := bufio.NewScanner(bytes.NewReader(d))
+	for sc.Scan() {
+		txt := sc.Text()
+		// To extract the log file path we split txt in a way that the path
+		// is the 2nd element.
+		// The string we're parsing:
+		// logger.go:103: Full logs written to /tmp/testing-logger-1901210787.log
+		if strings.Contains(txt, "Full logs written to") {
+			split := strings.Split(txt, "Full logs written to ")
+			if len(split) != 2 {
+				t.Fatalf("could not parse log file form test output, invalid format %q", txt)
+			}
+			path = split[1]
+			t.Cleanup(func() {
+				if t.Failed() {
+					t.Logf("Log file %q", path)
+				}
+			})
+		}
+	}
+
+	// Finally ensure the log file was kept and has the correct number of lines
+	f, err := os.Open(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			t.Fatalf("log file %q not found after the test exited", path)
+		}
+		t.Fatalf("cannot open log file for reading: %s", err)
+	}
+	defer f.Close()
+
+	lineCount := 0
+
+	for sc := bufio.NewScanner(f); sc.Scan(); {
+		lineCount++
+	}
+
+	if got, want := lineCount, 3; got != want {
+		t.Fatalf("expecting log file to contain %d lines, got %d:", want, got)
 	}
 }
