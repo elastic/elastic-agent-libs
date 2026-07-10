514d8b30edbc3c065241e2deac17acd0f4eddc94
Khushi Jain <khushi.jain@elastic.co>
2026-07-10 15:58:16 +0530

    Remove global logger from tlscommon package

--- transport/tlscommon/config.go
+++ transport/tlscommon/config.go
@@ -65,13 +65,13 @@ func LoadTLSConfig(config *Config, logger *logp.Logger) (*TLSConfig, error) {
 	var rootCAs certPoolProvider
 
 	if len(config.CAs) > 0 && config.CertificateReload.IsEnabled() {
-		reloader, err := NewCAReloader(config.CAs, config.CertificateReload.ReloadInterval)
+		reloader, err := NewCAReloader(config.CAs, config.CertificateReload.ReloadInterval, logger)
 		logFail(err)
 		if reloader != nil {
 			rootCAs = reloader
 		}
 	} else if len(config.CAs) > 0 {
-		pool, errs := LoadCertificateAuthorities(config.CAs)
+		pool, errs := LoadCertificateAuthorities(config.CAs, logger)
 		logFail(errs...)
 		rootCAs = newStaticCertPool(pool)
 	}
@@ -95,7 +95,7 @@ func LoadTLSConfig(config *Config, logger *logp.Logger) (*TLSConfig, error) {
 		reloader, err = NewCertReloader(config.Certificate.Certificate, config.Certificate.Key, logger, reloadOpts...)
 		logFail(err)
 	} else {
-		cert, err := LoadCertificate(&config.Certificate)
+		cert, err := LoadCertificate(&config.Certificate, logger)
 		logFail(err)
 		if cert != nil {
 			certs = []tls.Certificate{*cert}
--- transport/tlscommon/server_config.go
+++ transport/tlscommon/server_config.go
@@ -93,13 +93,13 @@ func LoadTLSServerConfig(config *ServerConfig, logger *logp.Logger) (*TLSConfig,
 	var clientCAs certPoolProvider
 
 	if len(config.CAs) > 0 && config.CertificateReload.IsEnabled() {
-		reloader, err := NewCAReloader(config.CAs, config.CertificateReload.ReloadInterval)
+		reloader, err := NewCAReloader(config.CAs, config.CertificateReload.ReloadInterval, logger)
 		logFail(err)
 		if reloader != nil {
 			clientCAs = reloader
 		}
 	} else if len(config.CAs) > 0 {
-		pool, errs := LoadCertificateAuthorities(config.CAs)
+		pool, errs := LoadCertificateAuthorities(config.CAs, logger)
 		logFail(errs...)
 		clientCAs = newStaticCertPool(pool)
 	}
@@ -118,7 +118,7 @@ func LoadTLSServerConfig(config *ServerConfig, logger *logp.Logger) (*TLSConfig,
 		reloader, err = NewCertReloader(config.Certificate.Certificate, config.Certificate.Key, logger, reloadOpts...)
 		logFail(err)
 	} else {
-		cert, err := LoadCertificate(&config.Certificate)
+		cert, err := LoadCertificate(&config.Certificate, logger)
 		logFail(err)
 		if cert != nil {
 			certs = []tls.Certificate{*cert}
